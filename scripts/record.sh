756dc56dd6f0db90fef491b19544c66cda1ff800
Andreas <andlydakis@gmail.com>
2016-06-02 15:14:23 -0400

    hm

--- scripts/record.sh
+++ scripts/record.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+#rosrun threespace_ros single_publisher.py
+#rosrun usb_cam usb_cam_node _framerate:=20
 sleep 15
 rosbag record -O $1 -q --duration=$2m --regex /l_hand_data_vec /r_hand_data_vec /l_upper_arm_data_vec /usb_camera_info /usb_cam/image_raw 
