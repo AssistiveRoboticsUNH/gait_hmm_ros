334ddd177d3b694a61a7b03b264fa02f3ee2bc91
Andreas Lydakis (local) <andlydakis@gmail.com>
2016-06-03 14:39:50 -0400

    annotated rec8

--- scripts/annotate.py
+++ scripts/annotate.py
@@ -16,12 +16,13 @@ from sensor_msgs.msg import Image
 
 from cv_bridge import CvBridge, CvBridgeError
 
-DataEntry = namedtuple('DataEntry','quatx quaty quatz quatw \
-            gyrox gyroy gyroz\
-            accelx accely accelz\
-            compx compy compz\
-            label,\
-            sequence')
+DataEntry = namedtuple('DataEntry',\
+        'quatx quaty quatz quatw \
+        gyrox gyroy gyroz \
+        accelx accely accelz \
+        compx compy compz \
+        label \
+        sequence')
 
 rospy.init_node('annotate')
 pref = sys.argv[1]
@@ -47,7 +48,7 @@ if(len(sys.argv)==3):
             upper_leg[i] = upper_leg[i]._replace(label = -1)
         i = 0
 #cv.NamedWindow("Annotation Window", 1)
-keys =[27,81,82,83,84,114,115]
+keys =[27, 81, 82, 83, 84, 114, 115, 99]
 labels = ['HO', "FF", "HS", "SW"]
 while i < len(images):
     print("Frame #"+str(i))
@@ -56,9 +57,11 @@ while i < len(images):
     #cv2.imshow("Annotation Window", images[i])
     cv2.imshow("Annotation Window", vis)
     k = cv2.waitKey(0)
+    print k&255
     while k&255 not in keys:
         print("Waiting for correct key")
         k = cv2.waitKey(0)
+        print k&255
     print("Key : " + str(k&255))
     #k = cv2.waitKey(0)
     k = k&255
@@ -71,7 +74,7 @@ while i < len(images):
         lower_leg[i] = lower_leg[i]._replace(label = 0)
         upper_leg[i] = upper_leg[i]._replace(label = 0)
         foot[i] = foot[i]._replace(label = 0)
-        print("Label : "+ labels[foot[i].label])
+        rospy.logwarn("Label : %s", labels[foot[i].label])
         i += 1
     elif k == 82:
         #UP
@@ -79,7 +82,7 @@ while i < len(images):
         lower_leg[i] = lower_leg[i]._replace(label = 1)
         upper_leg[i] = upper_leg[i]._replace(label = 1)
         foot[i] = foot[i]._replace(label = 1)
-        print("Label : "+ labels[foot[i].label])
+        rospy.logwarn("Label : %s", labels[foot[i].label])
         i += 1
     elif k == 83:
         #RIGHT
@@ -87,7 +90,7 @@ while i < len(images):
         lower_leg[i] = lower_leg[i]._replace(label = 2)
         upper_leg[i] = upper_leg[i]._replace(label = 2)
         foot[i] = foot[i]._replace(label = 2)
-        print("Label : "+ labels[foot[i].label])
+        rospy.logwarn("Label : %s ",labels[foot[i].label])
         i += 1
     elif k == 84:
         #DOWN
@@ -95,23 +98,26 @@ while i < len(images):
         lower_leg[i] = lower_leg[i]._replace(label = 3)
         upper_leg[i] = upper_leg[i]._replace(label = 3)
         foot[i] = foot[i]._replace(label = 3)
-        print("Label : "+ labels[foot[i].label])
+        rospy.logwarn("Label : %s ",labels[foot[i].label])
         i += 1
     elif k == 114:
         #GO BACK 10 FRAMES
-        print("Rewind")
+        rospy.logerr("Rewind")
         i = i-10
         if i < 0:
             i = 0
+    elif k == 99:
+        rospy.logwarn("Skipped Frame")
+        i += 1
     elif k == 115:
         #SAVE
-        print("Save")
         #pickle.dump(lower_leg, open(pref+"_lower_leg_annotated.p","wb"))
         #pickle.dump(upper_leg, open(pref+"_upper_leg_annotated.p","wb"))
         #pickle.dump(foot, open(pref+"_foot_annotated.p","wb"))
         pickle.dump(lower_leg, open(pref+"_lower_leg.p","wb"))
         pickle.dump(upper_leg, open(pref+"_upper_leg.p","wb"))
         pickle.dump(foot, open(pref+"_foot.p","wb"))
+        rospy.logerr("Saved")
         #i = i-1
         if i < 0:
             i=0
