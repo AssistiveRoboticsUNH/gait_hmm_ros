712631b306af8e91641cb16afffa654aa98ebcaa
Andreas <andlydakis@gmail.com>
2016-07-26 11:15:37 -0400

    pub

--- scripts/arduino_listener.py
+++ scripts/arduino_listener.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import time
 import sys
+import rospy
 # import config
 import serial
 import numpy as np
-# from gait_hmm_ros.msg import ardu_msg
+from gait_hmm_ros.msg import ardu_msg
 from xbee import XBee
 from xbee import ZigBee
 
@@ -14,16 +15,15 @@ data = ardu_msg()
 
 def make_msg(frame):
     # data = frame
-    fr_data = frame['rf_data']
-    data.sequ = fr_data[0]
-    data.ir = fr_data[1]
-    data.prox = fr_data[2]
-    data.fsrfl = fr_data[3]
-    data.fsrfr = fr_data[4]
-    data.fsrbk = fr_data[5]
-
-
-    print frame
+    fr_data = (frame['rf_data'][1:-1]).split(",")
+    print fr_data[0]
+    data.sequ = int(fr_data[0])
+    data.ir = float(fr_data[1])
+    data.prox = float(fr_data[2])
+    data.fsrfl = int(fr_data[3])
+    data.fsrfr = int(fr_data[4])
+    data.fsrbk = int(fr_data[5])
+    print frame['rf_data']
 
 
 # PORT = rospy.get_param('~xbee_port', '/dev/ttyUSB0')
@@ -42,7 +42,7 @@ dev = ZigBee(ser, escaped=True)
 # dev = XBee(ser, callback=make_msg)
 # dev = ZigBee(ser, callback=make_msg)
 
-arduPub = rospy.Publisher('arduino', gait_hmm_ros.msg.ardu_msg, queue_size=50)
+arduPub = rospy.Publisher('arduino', ardu_msg, queue_size=50)
 r = rospy.Rate(50)
 message_received = 0
 
