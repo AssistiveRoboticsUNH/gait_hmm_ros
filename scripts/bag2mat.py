1671fb66d80baae8bdaec682d72039d000094099
AndLydakis <andlydakis@gmail.com>
2016-08-02 16:54:18 -0400

    matlab scripts for fsr

--- scripts/bag2mat.py
+++ scripts/bag2mat.py
@@ -9,49 +9,231 @@
 #  modification, are permitted provided that the following conditions
 #  are met:
 #
-#   * Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
-#   * Redistributions in binary form must reproduce the above
-#     copyright notice, this list of conditions and the following
-#     disclaimer in the documentation and/or other materials provided
-#     with the distribution.
-#   * Neither the name of the University of California nor the names of its
-#     contributors may be used to endorse or promote products derived
-#     from this software without specific prior written permission.
-#
-#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-#  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-#  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-#  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-#  POSSIBILITY OF SUCH DAMAGE.
-
-usage_text = \
-"""
-bag2mat.py: a simple bagfile to MATLAB (version 5) .mat file converter.
-Usage:
-bag2mat.py bagfile.bag matfile.mat
-bag2mat.py -h # display this message
-"""
-
-def usage():
-  print usage_text
-  
+#   * Redistributions of source code must#!/usr/bin/env python
+import roslib
+import rospy
+import tf
+import tf2_ros
+import rosbag
+import rospkg
+import pickle
+import geometry_msgs.msg
+import sensor_msgs.msg
+import std_msgs.msg
+import time
+import math
+import string
+import pickle
 import sys
-from load_bag import BagLoader
-if '-h' in sys.argv:
-  usage()
-else:
-  try:
-    bagfile, matfile = sys.argv[1:3]
-    print "Converting %s -> %s" % (bagfile, matfile)
-    bag = BagLoader(bagfile)
-    bag.save_mat(matfile)  
-  except ValueError:
-    usage()
+import cv2
+import numpy
+import scipy.io as sio
+from threespace_ros.msg import dataVec
+from collections import namedtuple
+from cv_bridge import CvBridge, CvBridgeError
+
+DataEntry = namedtuple('DataEntry', 'quatx quaty quatz quatw \
+        gyrox gyroy gyroz \
+        accelx accely accelz \
+        compx compy compz \
+        label \
+        sequence')
+rospy.init_node('bag2pickle')
+pref = rospy.get_param('~prefix', "none")
+if pref == "none":
+    rospy.logerr("No input file given, exiting")
+    exit()
+rospack = rospkg.RosPack()
+
+pref = rospack.get_path('gait_hmm_ros')+'/scripts/'+pref
+
+bag = rosbag.Bag(pref+'.bag')
+bridge = CvBridge()
+
+matfile = rospy.get_param('~matfile', "none")
+if matfile != "none":
+    matfile_data = sio.loadmat(matfile)
+
+joint_names = ['rf', 'rll', 'rul',
+               'lf', 'lll', 'lul',
+               'm', 'ch', 'h',
+               'rs', 'rua', 'rla', 'rh',
+               'ls', 'lua', 'lla', 'lh']
+
+joint_names_full = ['Right Foot', 'Right Lower Leg', 'Right Upper Leg',
+                    'Left Foot', 'Left Lower Leg', 'Left Upper Leg',
+                    'Mid', 'Chest', 'Head',
+                    'Right Shoulder', 'Right Upper Arm', 'Right Lower Arm', 'Right Hand',
+                    'Left Shoulder', 'Left Upper Arm', 'Left Lower Arm', 'Left Hand']
+
+imu_names = ['~rf', '~rll', '~rul',
+             '~lf', '~lll', '~lul',
+             '~m', '~ch', '~h',
+             '~rs', '~rua', '~rla', '~rh',
+             '~ls', '~lua', '~lla', '~lh']
+
+imu_vectors = [[] for i in range(0, len(imu_names))]
+imu_pickle_data = [[] for i in range(0, len(imu_names))]
+imu_timestamps = [[] for i in range(0, len(imu_names))]
+imu_indices = [[] for i in range(0, len(imu_names))]
+imu_enable = [0 for i in range(0, len(imu_names))]
+imu_topics = ["" for i in range(0, len(imu_names))]
+
+images = []
+image_timestamps = []
+image_indices = []
+images_pickle_data = []
+timestamps = []
+pickle_timestamps = []
+arduino_messages = []
+arduino_timestamps = []
+arduino_indices = []
+arduino_pickle_data = []
+
+
+for i in range(0, len(imu_names)):
+    # rospy.logwarn(i)
+    # rospy.logwarn(len(joint_names_full))
+    # rospy.logwarn(len(imu_topics))
+    # rospy.logwarn(len(imu_names))
+    imu_topics[i] = rospy.get_param(imu_names[i], imu_enable[i])
+    rospy.logwarn(joint_names_full[i]+" topic : "+imu_topics[i])
+
+for topic, msg, t in bag.read_messages():
+    # rospy.logerr(topic)
+    if topic == "/usb_cam/image_raw":
+        # rospy.logerr(msg.header.stamp)
+        # rospy.logerr("/usb_cam/image_raw")
+        images.append(msg)
+        image_timestamps.append(t.to_nsec())
+    elif topic == "/arduino":
+        arduino_messages.append(msg)
+        arduino_timestamps.append(t.to_nsec())
+    else:
+        index = imu_topics.index(topic[1:])
+        # rospy.logerr(topic+" --> "+imu_names[index])
+        # rospy.logerr(msg.header.stamp)
+        imu_vectors[index].append(msg)
+        imu_timestamps[index].append(t.to_nsec())
+
+min_ = sys.maxint
+min_index = 0
+
+for i in range(0, len(imu_vectors)):
+    rospy.logwarn(str(len(imu_vectors[i]))+" Frames in "+imu_names[i])
+    if len(imu_vectors[i]) != 0:
+        if len(imu_vectors[i]) < min_:
+            min_ = len(imu_vectors[i])
+            min_index = i
+# for topic, msg, t in bag.read_messages():
+for i in range(0, min_):
+    ts = int(imu_timestamps[min_index][i])
+    # print ts
+    indexes = []
+    time = 0.0
+    n = 0.0
+    for j in range(0, len(imu_vectors)):
+        # t = imu_timestamps[j].index(min(imu_timestamps[j], key=lambda x: abs(x-ts)))
+        if(len(imu_vectors[j])) != 0:
+            t = imu_timestamps[j].index(min(imu_timestamps[j], key=lambda x: abs(x-ts)))
+            # print t
+            imu_indices[j].append(t)
+            time += imu_timestamps[j][t]
+
+            n += 1.0
+        else:
+            imu_indices[j].append(-1)
+    # image_indexes.append(images.index(images[(min(image_timestamps, key=lambda x: abs(x-ts)))]))
+    pickle_timestamps.append(time/n)
+    image_indices.append(image_timestamps.index((min(image_timestamps, key=lambda x: abs(x-ts)))))
+    arduino_indices.append(arduino_timestamps.index((min(arduino_timestamps, key=lambda x: abs(x-ts)))))
+
+    # rospy.logerr(str(i)+"/"+str(len(imu_vectors[min_index]))+":"+str(ts)+" --> "+str(t))
+
+# for j in range(0, len(imu_vectors)):
+#     print len(imu_indices[j])
+# print len(image_indices)
+
+for i in range(0, min_):
+    for j in range(0, len(imu_vectors)):
+        if len(imu_vectors[j]) != 0:
+            # print imu_indices[j][i]
+            data_ = imu_vectors[j][imu_indices[j][i]]
+            p_data_ = DataEntry(data_.quat.quaternion.x, data_.quat.quaternion.y,
+                                data_.quat.quaternion.z, data_.quat.quaternion.w,
+                                data_.gyroX, data_.gyroY, data_.gyroZ,
+                                data_.accX, data_.accY, data_.accZ,
+                                data_.comX, data_.comY, data_.comZ,
+                                -1,
+                                i)
+            # print p_data_
+            # imu_pickle_data[j].append(p_data_)
+            imu_pickle_data[j].append([data_.quat.quaternion.x, data_.quat.quaternion.y,
+                                       data_.quat.quaternion.z, data_.quat.quaternion.w,
+                                       data_.gyroX, data_.gyroY, data_.gyroZ,
+                                       data_.accX, data_.accY, data_.accZ,
+                                       data_.comX, data_.comY, data_.comZ,
+                                       -1,
+                                       i])
+
+    ard = arduino_messages[arduino_indices[i]]
+    arduino_pickle_data.append([ard.header.stamp.secs, ard.header.stamp.nsecs, ard.sequ, ard.fsrbk, ard.fsrfl,
+                                ard.fsrfr, ard.ir, ard.prox])
+    # arduino_pickle_data.append(arduino_messages[arduino_indices[i]])
+
+    # try:
+    #    cvim = bridge.imgmsg_to_cv2(images[image_indices[i]], "bgr8")
+    #    images_pickle_data.append(cvim)
+    # except CvBridgeError as e:
+    #    print(e)
+# for j in range(0, len(imu_vectors)):
+#     print len(imu_vectors[j])
+# print len(image_indices)
+
+# for j in range(0, len(imu_vectors)):
+#    print len(imu_pickle_data[j])
+# print len(images_pickle_data)
+
+rospy.logerr(str(len(image_indices))+" images")
+rospy.logerr(str(len(arduino_indices))+" arduino readings")
+rospy.logwarn(str(min_)+" total frames")
+
+for j in range(0, len(imu_vectors)):
+    if len(imu_pickle_data[j]) != 0:
+        name = pref + "_" + joint_names[j] + ".mat"
+        rospy.logwarn("dumping " + imu_names[j] + " to " + name)
+        # pickle.dump(imu_pickle_data[j], open(name, "wb"))
+        sio.savemat(name, mdict={joint_names[j]: imu_pickle_data[j]})
+        # sio.savemat(name, imu_pickle_data[j])
+# print pickle_timestamps[80:100]
+# print "----------------------"
+# print imu_timestamps[3][80:100]
+# print len(pickle_timestamps)
+rospy.logwarn("dumping timestamps to " + pref + "_timestamps.mat")
+# pickle.dump(pickle_timestamps, open(pref + "_timestamps.p", "wb"))
+sio.savemat(pref + "_timestamps.mat", mdict={"timestamps": pickle_timestamps})
+# sio.savemat(pref + "_timestamps.mat", pickle_timestamps)
+
+
+rospy.logwarn("dumping image indices to " + pref + "_im_index.mat")
+# pickle.dump(pickle_timestamps, open(pref + "_timestamps.p", "wb"))
+sio.savemat(pref + "_im_index.mat", mdict={"im_ind": image_indices})
+# sio.savemat(pref + "_im_index.mat", image_indices)
+
+rospy.logwarn("dumping arduino indices to " + pref + "_ar_index.mat")
+# pickle.dump(pickle_timestamps, open(pref + "_timestamps.p", "wb"))
+sio.savemat(pref + "_ar_index.mat", mdict={"ar_ind": arduino_indices})
+# sio.savemat(pref + "_ar_index.mat", arduino_indices)
+
+# rospy.logwarn("dumping images to " + pref + "_images.p")
+# pickle.dump(images, open(pref + "_images.p", "wb"))
+
+rospy.logwarn("dumping arduino readings to " + pref + "_arduino.mat")
+# pickle.dump(arduino_messages, open(pref + "_arduino.p", "wb"))
+sio.savemat(pref + "_arduino.mat", mdict={'arduino': arduino_pickle_data})
+# sio.savemat(pref + "_arduino.mat", arduino_messages)
+# sio.savemat(pref + "_arduino.mat", arduino_pickle_data)
+
+# rospy.logerr(str(len(images_pickle_data))+" frames")
+
+
---
+++ scripts/fsr_annotate.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+import roslib
+import rospy
+import rospkg
+import pickle
+import time
+import math
+import string
+import sys
+import cv2
+import os.path
+import imu_callbacks as iparam
+import geometry_msgs.msg
+import std_msgs.msg
+import sensor_msgs.msg
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from collections import namedtuple
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from datetime import datetime
+
+DataEntry = namedtuple('DataEntry',
+                       'quatx quaty quatz quatw \
+                        gyrox gyroy gyroz \
+                        accelx accely accelz \
+                        compx compy compz \
+                        label \
+                        sequence')
+
+
+def classify(arduino_reading):
+    # fsrbk fsrfl fsrfr
+    # print(str(arduino_reading[4])+" "+str(arduino_reading[4]) + " " + str(arduino_reading[4]))
+    if (arduino_reading[4] == 1 and arduino_reading[5] == 1 and arduino_reading[6] == 1) or \
+            (arduino_reading[4] == 1 and arduino_reading[5] == 0 and arduino_reading[6] == 1):
+        print('stance')
+        return 'stance'
+    elif arduino_reading[4] == 0:
+        print('swing')
+        return 'swing'
+    elif arduino_reading[4] == 1 and arduino_reading[5] == 0 and arduino_reading[6] == 0:
+        print('stance')
+        return 'stance'
+    else:
+        print('stance')
+        return 'stance'
+
+
+rospy.init_node('auto_annotate')
+pref = rospy.get_param('~prefix', "none")
+auto = rospy.get_param('~auto', "False")
+rospack = rospkg.RosPack()
+path = rospack.get_path('gait_hmm_ros') + '/scripts/'
+pref = path + pref
+
+matfile = rospy.get_param('~matfile', "none")
+if matfile != "none":
+    matfile_data = sio.loadmat(path + matfile)
+
+leg = rospy.get_param("~leg", "left")
+
+joint_names = iparam.imu_names
+# ['rf', 'rll', 'rul', 'lf', 'lll', 'lua', 'lul', 'm', 'ch', 'rs', 'rua', 'rla',
+#                'rw', 'ls', 'lua', 'lla', 'lw']
+
+imu_names = iparam.imu_param_names
+# ['~rf', '~rll', '~rul', '~lf', '~lll', '~lua', '~lul', '~m', '~ch', '~rs', '~rua', '~rla',
+#          '~rw', '~ls', '~lua', '~lla', '~lw']
+
+joint_names_full = iparam.imu_names_full
+
+# joint_names_full = ['Right Foot', 'Right Lower Leg', 'Right Upper Leg',
+#                    'Left Foot', 'Left Lower Leg', 'Left Upper Leg',
+#                    'Mid', 'Chest',
+#                    'Right Shoulder', 'Right Upper Arm', 'Right Lower Arm', 'Right Wrist',
+#                    'Left Shoulder', 'Left Upper Arm', 'Left Lower Arm', 'Left Wrist']
+
+imu_pickled_data = []
+arduino_pickled_data = []
+gait_classification = []
+
+total_entries = 0
+total_sensors = len(imu_names)
+
+#####################
+# Load enabled IMUS #
+#####################
+for name in joint_names:
+    fullname = pref + "_" + name + ".mat"
+    if os.path.isfile(fullname):
+        rospy.logwarn("Loading " + fullname)
+        # x = pickle.load(open(fullname, "rb"))
+        x = sio.loadmat(fullname)[name]
+        # print x
+        print len(x)
+        imu_pickled_data.append(x)
+        total_entries = len(x)
+    else:
+        imu_pickled_data.append([])
+
+rospy.logwarn("Loading timestamps from " + pref + "_timestamps.mat")
+# imu_timestamps = pickle.load(open(pref + "_timestamps.mat", "rb"))
+imu_timestamps = sio.loadmat(pref + "_timestamps.mat")['timestamps']
+print imu_timestamps
+rospy.logwarn("Loading arduino data from " + pref + "_arduino.mat")
+arduino_pickled_data = sio.loadmat(pref + "_arduino.mat")['arduino']
+
+rospy.logwarn("Loading arduino indices from " + pref + "_ar_index.mat")
+arduino_indices = sio.loadmat(pref + "_ar_index.mat")['ar_ind']
+
+# rospy.logwarn("Loading image indices from " + pref + "_im_index.mat")
+# image_indices = sio.loadmat(pref + "_im_index.mat")
+
+# rospy.logwarn("Loading images from " + pref + "_images.p")
+# images = pickle.load(open(pref + "_images.p", "rb"))
+# rospy.logwarn("Loading arduino indices from " + pref + "_indices.p")
+# indices = pickle.load(open(pref + "_indices.p", "rb"))
+rl_timestamps = []
+
+#############################################
+# Transform ROS timestamps to duration from #
+# start of recording                        #
+#############################################
+for i in imu_timestamps:
+    rl_timestamps.append(abs(i - imu_timestamps[0]) / 1000000000)
+
+# start_frame = min(matfile_data['LHS'][0], matfile_data['LHS'], matfile_data['LHS'], matfile_data['LHS'][0])
+mocap_data = []
+class_pickle = []
+mocap_labels = ['LHS', 'LTO', 'RHS', 'RTO']
+mocap_indexes = [0, 0, 0, 0]
+phase_labels_a = ['swing', 'stance']
+phase_indices_a = [0, 1]
+phase_labels_b = ['lswing', 'lstance', 'rswing', 'rstance']
+
+for i in range(0, total_entries):
+    # rospy.loginfo("#"+str(i)+": Lower Index :"+str(lower_index)+", Upper Index :"+str(upper_index))
+    g = classify(arduino_pickled_data[i])
+    class_pickle.append(phase_labels_a.index(g))
+
+rospy.logwarn(str(len(class_pickle)) + " entries annotated")
+rospy.logwarn("Dumping labels to " + pref + "_labels_annotated.mat")
+sio.savemat(pref + "_labels_annotated.mat", mdict={'labels': class_pickle})
